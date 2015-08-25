35a67020bdac6b26e4d99fb3ea574edee8c98c8e
YipManDan <Yeh.DanielP@gmail.com>
2015-08-25 12:04:38 -0700

    Created Clock

--- Main.java
+++ Main.java
@@ -5,12 +5,12 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    SwingUtilities.invokeLater(new Runnable() {
-        public void run() {
-            MainFrame ex = new MainFrame();
-            ex.setVisible(true);
-        }
-    });
+        new Clock();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                MainFrame ex = new MainFrame();
+                ex.setVisible(true);
+            }
+        });
     }
 }
