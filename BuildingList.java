13ca17fa681f29a479aff11a03a514d8182b5a27
YipManDan <Yeh.DanielP@gmail.com>
2015-09-02 11:57:01 -0700

    Removed unused code Added comments

--- BuildingList.java
+++ BuildingList.java
@@ -5,12 +5,18 @@ import java.time.LocalTime;
 import java.util.ArrayList;
 
 /**
- * Created by Daniel on 8/25/2015.
+ * Static class which contains the list of buildings
+ * Provides access to SubAreas, and Sensors through the Buildings
+ * Static enumerator: roomRef
  */
 public class BuildingList {
     public static ArrayList<Building> buildings = new ArrayList<>(2);
     public enum roomRef {KITCHEN, LIVINGROOM, CLOSET, ROOM1, ROOM2, BATHROOM}
 
+    /**
+     * Creator for BuildingList
+     * Cteates building, sets password, and supplies image URL
+     */
     BuildingList() {
         Building temp = new Building(1);
         temp.createSubAreas(6);
@@ -26,9 +32,19 @@ public class BuildingList {
         temp.setImage(url);
         buildings.add(temp);
     }
+
+    /**
+     * Function alows access to Building from list
+     * @param index the index of the building
+     * @return the building found at index
+     */
     public static Building getBuilding(int index) {
         return buildings.get(index);
     }
+
+    /**
+     * Generates the default sensor data
+     */
     public static void generateDefaults() {
         Building temp = BuildingList.getBuilding(0);
         SubAreas temp2 = temp.getSubArea(1);
@@ -44,10 +60,5 @@ public class BuildingList {
         temp2 = temp.getSubArea(6);
         temp2.createFireSensor();
         temp2.createMotionSensor();
-
-
     }
-
-
-
 }
